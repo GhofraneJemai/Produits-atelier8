63e746529008866991db20dec69687686a366f82
Ghofrane <jemaighofrane22@gmail.com>
2024-10-02 20:47:14 +0200

    first commit

--- src/app/add-produit/add-produit.component.ts
+++ src/app/add-produit/add-produit.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-produit',
-  templateUrl: './add-produit.component.html',
-  styleUrl: './add-produit.component.css'
+  templateUrl: './add-produit.component.html'
 })
-export class AddProduitComponent {
+export class AddProduitComponent implements OnInit {
+
+  newProduit = new Produit();
+  constructor(private produitService: ProduitService,
+              private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  addProduit() {
+    //console.log(this.newProduit);
+    this.produitService.updateProduit(this.newProduit);
+    this.router.navigate(['produits']);
+  }
 
 }
